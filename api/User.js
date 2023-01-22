51bf3910bf30565ae333aad78213bed8bb7d8d32
PetraCrmaric <petracrm@hotmail.com>
2023-01-22 12:42:42 +0100

    added jwt

--- api/User.js
+++ api/User.js
@@ -73,42 +73,64 @@ router.post('/register', async (req, res) => {
       name, surname, email, password,
     } = req.body;
 
-    // Validate user input
-    if (!(email && password && name && surname)) {
-      res.status(400).send('All input is required');
+    name = name.trim();
+    surname = surname.trim();
+    email = email.trim();
+    password = password.trim();
+
+    if (name == '' || surname == '' || email == '' || password == '') {
+      res.json({
+        status: 'FAILED',
+        message: 'Empty input fields',
+      });
+    } else if (!/^[a-zA-Z ]*$/.test(name)) {
+      res.json({
+        status: 'FAILED',
+        message: 'Invalid name entered',
+      });
+    } else if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
+      res.json({
+        status: 'FAILED',
+        message: 'Invalid email entered',
+      });
+    } else if (password.length < 5) {
+      res.json({
+        status: 'FAILED',
+        message: 'Password is too short!',
+      });
+
+      // check if user already exist
+      const oldUser = await User.findOne({ email });
+
+      if (oldUser) {
+        return res.status(409).send('User Already Exist. Please Login');
+      }
+
+      // Encrypt user password
+      encryptedPassword = await bcrypt.hash(password, 10);
+
+      // Create user in our database
+      const user = await User.create({
+        name,
+        surname,
+        email: email.toLowerCase(),
+        password: encryptedPassword,
+      });
+
+      // Create token
+      const token = jwt.sign(
+        { user_id: user._id, email },
+        process.env.TOKEN_KEY,
+        {
+          expiresIn: '2h',
+        },
+      );
+      // save user token
+      user.token = token;
+
+      // return new user
+      res.status(201).json(user);
     }
-
-    // check if user already exist
-    const oldUser = await User.findOne({ email });
-
-    if (oldUser) {
-      return res.status(409).send('User Already Exist. Please Login');
-    }
-
-    // Encrypt user password
-    encryptedPassword = await bcrypt.hash(password, 10);
-
-    // Create user in our database
-    const user = await User.create({
-      name,
-      surname,
-      email: email.toLowerCase(),
-      password: encryptedPassword,
-    });
-
-    // Create token
-    const token = jwt.sign(
-      { user_id: user._id, email },
-      process.env.TOKEN_KEY,
-      {
-        expiresIn: '2h',
-      },
-    );
-    // save user token
-    user.token = token;
-
-    // return new user
-    res.status(201).json(user);
   } catch (err) {
     console.log(err);
   }
@@ -145,144 +167,6 @@ router.post('/login', async (req, res) => {
     res.status(400).send('Invalid Credentials');
   } catch (err) {
     console.log(err);
-  }
-});
-
-// signup
-router.post('/signup', (req, res) => {
-  let {
-    name, surname, email, password,
-  } = req.body;
-  name = name.trim();
-  surname = surname.trim();
-  email = email.trim();
-  password = password.trim();
-
-  if (name == '' || surname == '' || email == '' || password == '') {
-    res.json({
-      status: 'FAILED',
-      message: 'Empty input fields',
-    });
-  } else if (!/^[a-zA-Z ]*$/.test(name)) {
-    res.json({
-      status: 'FAILED',
-      message: 'Invalid name entered',
-    });
-  } else if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
-    res.json({
-      status: 'FAILED',
-      message: 'Invalid email entered',
-    });
-  } else if (password.length < 5) {
-    res.json({
-      status: 'FAILED',
-      message: 'Password is too short!',
-    });
-  } else {
-    User.find({ email }).then((result) => {
-      if (result.length) {
-        res.json({
-          status: 'FAILED',
-          message: 'User with that email already exists ',
-        });
-      } else {
-        // try to create new user
-
-        // password handling
-        const saltRounds = 10;
-        bcrypt.hash(password, saltRounds).then((hashedPassword) => {
-          const newUser = new User({
-            name,
-            surname,
-            email,
-            password: hashedPassword,
-          });
-
-          newUser.save().then((result) => {
-            res.json({
-              status: 'SUCCESS',
-              message: 'Signup successful',
-              data: result,
-            });
-          })
-            .catch((err) => {
-              res.json({
-                status: 'FAILED',
-                message: 'An error occured while saving account ',
-              });
-            });
-        })
-          .catch((err) => {
-            res.json({
-              status: 'FAILED',
-              message: 'An error occured while hashing password ',
-            });
-          });
-      }
-    }).catch((err) => {
-      console.log(err);
-      res.json({
-        status: 'FAILED',
-        message: 'An error occured while checking for existing user ',
-      });
-    });
-  }
-});
-
-// signin
-router.post('/signin', (req, res) => {
-  let { email, password } = req.body;
-  email = email.trim();
-  password = password.trim();
-
-  if (email == '' || password == '') {
-    res.json({
-      status: 'FAILED',
-      message: 'Empty credentials',
-    });
-  } else {
-    // check if user exists
-    User.find({ email })
-      .then((data) => {
-        if (data) {
-          // user exists
-          const hashedPassword = data[0].password;
-          bcrypt.compare(password, hashedPassword).then(
-            (result) => {
-              if (result) {
-                // password match
-                res.json({
-                  status: 'SUCCESS',
-                  message: 'Signin successful',
-                  data,
-                });
-              } else {
-                res.json({
-                  status: 'FAILED',
-                  message: 'Invalid password entered',
-                });
-              }
-            },
-          )
-            .catch((err) => {
-              res.json({
-                status: 'FAILED',
-                message: 'An error occured while comparing passwords ',
-              });
-            });
-        } else {
-          res.json({
-            status: 'FAILED',
-            message: 'Invalid credentials ',
-          });
-        }
-      })
-      .catch((err) => {
-        res.json({
-          status: 'FAILED',
-          message: 'An error occurred while checking for existing user ',
-        });
-      });
   }
 });
 
